e5fce9d8ce418ebdcfa0da09c7e7eeb648b5b850
kmj <designshake74@gmail.com>
2024-05-22 17:12:57 +0900

    0522 각콤포넌트 다시작업

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//role 그 사이트의 역할같은걸 의미한다.
+
+const Header = () => {
+  return (
+    <Header id='header' role='nav'>
+       Headersss
+      </Header>
+  )
+}
+
+export default Header 
