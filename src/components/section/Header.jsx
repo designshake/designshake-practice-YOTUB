3b66fa0260d8e2775819f390f2b812e2c981cf52
kmj <designshake74@gmail.com>
2024-05-22 19:57:18 +0900

    구조화다시올림

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-//role 그 사이트의 역할같은걸 의미한다.
 
 const Header = () => {
   return (
-    <Header id='header' role='nav'>
-       Headersss
-      </Header>
+    <header id='header' role='banner'>
+      Header
+    </header>
   )
 }
 
-export default Header 
+export default Header
