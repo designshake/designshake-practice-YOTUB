6e379e676f527ea71e63fd1af785cb0d1253179d
kmj <designshake74@gmail.com>
2024-06-06 19:11:27 +0900

    0606 나머지섹션 작업

--- src/components/contents/Website.jsx
+++ src/components/contents/Website.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { webdText } from '../../data/webd'
+import { Link } from 'react-router-dom'
 
 const Website = () => {
   return (
-    <div>Website</div>
+    <section id='website'>
+      <h2>웹표준사이트 기초 다지기</h2>
+      <div className='video__inner'>
+          {webdText.map ((video, key) => (
+            <div className='video' key={key}>
+              <div className='video__thumb play__icon'>
+                <Link to={`/video/${video.videoId}`}>
+                  <img src={video.img} alt={video.title}/>
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
   )
 }
 
--- src/components/contents/Youtube.jsx
+++ src/components/contents/Youtube.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import { youtubeText } from '../../data/youtube'
+import { Link } from 'react-router-dom'
+
 const Youtube = () => {
   return (
-    <div>Youtube</div>
+    <section id='youtube'>
+      <h2>웹디자인기능사 준비는 이걸로.</h2>
+      <div className='video__inner'>
+          {youtubeText.map ((video, key) => (
+            <div className='video' key={key}>
+              <div className='video__thumb play__icon'>
+                <Link to={`/video/${video.videoId}`}>
+                  <img src={video.img} alt={video.title}/>
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
   )
 }
 
