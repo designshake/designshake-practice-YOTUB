223fef21888437b0b5eb10587266cb18a18dbfd9
kmj <designshake74@gmail.com>
2024-06-15 19:02:43 +0900

    0615 비디오상세페이지

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -17,6 +17,7 @@ const Search = () => {
       title='서치페이지'
       description='검색결과페이지 입니다.'
     >
+      <h2><em>검색결과입니다.</em></h2>
         <seaction id='searchPage'>
           <div className='video__inner search'>
             <VideoSearch />
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Main from '../components/section/Main'
 
 const Video = () => {
+
   return (
     <Main 
       title='비디오페이지입니다.'
       description='비디오안내페이지입니다.'
     >
-      Video
+      <section>
+        main 이 부분은 유튜브 만들기 part20. 개발파트영역.
+      </section>
     </Main>
   )
 }
